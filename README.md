ed52c135dfc4f64e04bc944c73f2a4a8c20a39bd
Sammy Pfeiffer <sammypfeiffer@gmail.com>
2019-05-22 17:08:24 +1000

    Initial push

---
+++ README.md
@@ -0,0 +1,18 @@
+# Team 2easy repository
+
+This repository contains the code from the team "2easy" composed by:
+
+* Sammy Pfeiffer (Sammy.Pfeiffer at student.uts.edu.au)
+* Eduardo
+* ...
+
+
+# Projects
+
+* `competition_sm`: Contains the competition state machine. We divided the state machine in missions, every mission is a state machine. A general state machine executes all the isolated state machines.
+* `traffic_light_detector`:
+* `stop_signal_detector`:
+* `pedestrian_detector`:
+
+
+
---
+++ competition_sm/src/competition_sm/mission_1.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+
+import rospy
+import smach
+from std_msgs.msg import String
+
+
+class MoveUntilTrafficLight(smach.State):
+    def __init__(self):
+        smach.State.__init__(self, outcomes=['succeeded', 'failed'])
+
+    def execute(self, userdata):
+        rospy.loginfo('Executing state ' + self.__class__.__name__)
+        # Send a goal to our "Move using waypoints" server and wait until
+        # we reach the goal
+
+        rospy.sleep(3)
+        return 'succeeded'
+        # if something went wrong
+        # return 'failed'
+
+
+class WaitForTrafficLightGreenStatus(smach.State):
+    def __init__(self):
+        smach.State.__init__(self, outcomes=['got_green'])
+        self.got_green = False
+        self._green_subscriber = rospy.Subscriber('/traffic_light_status',
+                                                  String,
+                                                  queue_size=1)
+
+    def traffic_light_status_cb(self, msg):
+        status = msg.data.upper()
+        if status.contains('g'):
+            self.got_green = True
+
+    def execute(self, userdata):
+        rospy.loginfo('Executing state ' + self.__class__.__name__)
+        while not rospy.is_shutdown() and not self.got_green:
+            rospy.sleep(0.1)
+        return 'got_green'
+
+
+def mission_1():
+
+    # Create a SMACH state machine
+    sm = smach.StateMachine(outcomes=['succeeded', 'failed'])
+
+    # Open the container
+    with sm:
+        # Add states to the container
+        smach.StateMachine.add('Move_until_traffic_light',
+                               MoveUntilTrafficLight(),
+                               transitions={'succeeded': 'Wait_for_traffic_light_green_status',
+                                            'failed': 'outcome4'})
+        smach.StateMachine.add('Wait_for_traffic_light_green_status',
+                               WaitForTrafficLightGreenStatus(),
+                               transitions={'outcome2': 'FOO'})
+
+    # Execute SMACH plan
+    outcome = sm.execute()
+
+
+if __name__ == '__main__':
+    rospy.init_node("mission_1_sm")
+    mission_1()
