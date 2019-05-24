eb4fca4869e7e98208bab2fa39b510748d756d26
Eduardo <eduardo.ferrera@uni-due.de>
2019-05-24 04:57:48 +0200

    Fresh start of day 3

---
+++ traffic_light_detector/README.md
@@ -0,0 +1,10 @@
+In order to make this system work, I am planing to use an adaptation of the following code:
+
+@ONLINE{cr,
+    author = "Ahmet Özlü",
+    title  = "Color Recognition",
+    year   = "2018",
+    url    = "https://github.com/ahmetozlu/color_recognition"
+}
+
+After the recognition of the darknet we use its segmentation and check the color 
--- traffic_light_detector/src/traffic_light_detector/traffic_light_detector_impl.py
+++ traffic_light_detector/src/traffic_light_detector/traffic_light_detector_impl.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
-# Author: Your name <youremail@gmail.com>
-
+# Author: Eduardo Ferrera <eferrera@catec.aero>
 
 # Python
 import numpy as np
@@ -12,30 +11,51 @@ from std_msgs.msg import String
 from sensor_msgs.msg import Image
 
 # Other imports
-
+import cv2
+from cv_bridge import CvBridge, CvBridgeError
 
 class TrafficLightDetector(object):
     """
-    Traffic Light Detector based on blabla
+    This traffic light detector tries to improve the output of Yolo in a crappy way.
+    Reduces the size of the image, to make things easier for Yolo
+    Detects the bounding boxes given by Yolo and checks the status of the traffic_light
     """
 
     def __init__(self):
-        self.pub = rospy.Publisher("/traffic_light_status",
-                                   String, queue_size=1)
-        rospy.loginfo("Publishing traffic light detections to: " +
-                      self.pub.resolved_name)
+        # Subscribing to the image
+        self.reduction_factor = 0.5
 
-        # Subscribe to images?
-        self.last_img = None
-        self.img_sub = rospy.Subscriber('/car_camera',
-                                        Image,
-                                        self.img_cb,
+        if (self.reduction_factor >= 1 or self.reduction_factor <= 0):
+            rospy.logwarn("Ignoring the reduction factor")
+            self.reduction_factor = 0
+
+        self.cv_last_img = None
+        self.bridge = CvBridge()
+        self.img_sub = rospy.Subscriber('/camera0/image_raw', Image, self.cropper2yolo_cb,
                                         queue_size=1)
-        rospy.loginfo("Subscribing to images at: " +
-                      self.img_sub.resolved_name)
+        rospy.loginfo("Subscribing to images at: " + self.img_sub.resolved_name)
 
-    def img_cb(self, img):
-        self.last_img = img
+        # Publishing new images prepared for Yolo
+        self.img_pub = rospy.Publisher("/camera0/image_for_yolo", Image, queue_size=1)
+        rospy.loginfo("Publishes the image to: /camera0/image_for_yolo")
+
+        # Receives feedback from Yolo (crops this area of the image)
+        #self.yolo_sub = rospy.Subscriber('/',, self.yolo_output_cb, queue_size=5)
+
+        # Publishing the result of the node
+        self.pub = rospy.Publisher("/traffic_light_status", String, queue_size=1)
+        rospy.loginfo("Publishing traffic light detections to: " + self.pub.resolved_name)
+
+
+    def cropper2yolo_cb(self, img):
+        # Cropping the image (makes it faster and easier for yolo) and publishing it
+        cv_img = self.bridge.imgmsg_to_cv2(img,desired_encoding="bgr8")
+        # Grabing the size of the image
+        (h, w) = cv_img.shape[:2]
+        rf = self.reduction_factor/2.0
+        cv_img = cv_img[int(rf*h):int((1-rf)*h),int(rf*w):int((1-rf)*w)]
+        self.cv_last_img = cv_img
+        self.img_pub.publish(self.bridge.cv2_to_imgmsg(cv_img, encoding="bgr8"))
 
     def detect_traffic_light(self, image):
         """
@@ -61,8 +81,8 @@ class TrafficLightDetector(object):
     def run(self):
         rate = rospy.Rate(10)  # Detect at 10hz
         while not rospy.is_shutdown():
-            if self.last_img is not None:
-                result = self.detect_traffic_light(self.last_img)
+            if self.cv_last_img is not None:
+                result = self.detect_traffic_light(self.cv_last_img)
                 self.pub_traffic_light_status(result)
             rate.sleep()
 
