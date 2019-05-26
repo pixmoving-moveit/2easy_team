a361dc6bfeb96cc08b1d9210022aa1bc64e5dd07
Eduardo <eduardo.ferrera@uni-due.de>
2019-05-26 09:01:24 +0200

    Last version working before cropping

--- stop_detector/src/stop_detector/stop_detector_impl.py
+++ stop_detector/src/stop_detector/stop_detector_impl.py
@@ -8,9 +8,14 @@ import numpy as np
 import rospy
 from std_msgs.msg import String
 from sensor_msgs.msg import Image
+from darknet_ros_msgs.msg import BoundingBoxes
 
 # Autoware message
 from autoware_msgs.msg import DetectedObjectArray
+
+# Other imports
+import cv2
+from cv_bridge import CvBridge, CvBridgeError
 
 
 class StopDetector(object):
@@ -19,10 +24,14 @@ class StopDetector(object):
     """
 
     def __init__(self):
+        # It only pays attention to the stop sign if is at the right
+        self.reduction_factor = 0.5
+
         self.pub = rospy.Publisher("/stop_detection_status",
                                    String, queue_size=1)
         rospy.loginfo("Publishing traffic stop detections to: " +
                       self.pub.resolved_name)
+        self.bridge = CvBridge()
 
         # stop_sign status
         self.last_obj = None
@@ -32,8 +41,13 @@ class StopDetector(object):
         self.h_ss = None
         self.w_ss = None       
 
-        self.ped_sub = rospy.Subscriber('detection/image_detector/objects', 
-                                        DetectedObjectArray, self.obj_cb, queue_size=1)
+
+        self.ped_sub_yolo3 = rospy.Subscriber('detection/image_detector/objects', 
+                                        DetectedObjectArray, self.yolo3_cb, queue_size=1)
+
+        self.img2yolo = None
+        self.ped_sub_yolo1 = rospy.Subscriber('/darknet_ros/bounding_boxes', 
+                                        BoundingBoxes, self.yolo1_cb, queue_size=1)
 
         # Publishing new images prepared for Yolo
         self.img_sub = rospy.Subscriber('/camera0/image_raw', Image,                                        self.img2yolo_cb,
@@ -42,8 +56,31 @@ class StopDetector(object):
         self.img2yolo_pub = rospy.Publisher("/camera0/image_for_yolo", Image, queue_size=1)
         rospy.loginfo("Publishes the image to: /camera0/image_for_yolo")
 
+        self.img_seg_pub = rospy.Publisher("camera0/traffic_light_image", Image, queue_size=1)
 
-    def obj_cb(self, DetectedObject):   
+    def yolo1_cb(self, DetectedObject):  
+        self.ped_label = None
+        self.x = None
+        self.y = None
+        self.h_ss = None
+        self.w_ss = None
+        do_print = False
+        for obj in DetectedObject.bounding_boxes:
+            if obj.Class == 'stop sign': 
+                self.label_ss = obj.Class
+                self.x = obj.xmin
+                self.y = obj.ymin
+                self.h_ss = obj.xmax - obj.xmin
+                self.w_ss = obj.ymax - obj.ymin 
+                do_print = True
+
+        if (do_print):
+            img = self.img2yolo
+            cv_img = self.bridge.imgmsg_to_cv2(img,desired_encoding="bgr8")
+            cv_img = cv_img[self.y:(self.y + self.w_ss),self.x:(self.x + self.h_ss)]
+            self.img_seg_pub.publish(self.bridge.cv2_to_imgmsg(cv_img, encoding="bgr8"))
+
+    def yolo3_cb(self, DetectedObject): 
         self.ped_label = None
         self.x = None
         self.y = None
@@ -54,8 +91,6 @@ class StopDetector(object):
                 self.label_ss = obj.label
                 self.x_ss = obj.x
                 self.y_ss = obj.y
-                self.h_ss = obj.height
-                self.w_ss = obj.width
                 # print (self.x_ss)
                 
         self.last_obj = DetectedObject.objects
@@ -67,7 +102,7 @@ class StopDetector(object):
         returns 'STOP'
         """
         # Do magic code here
-        if self.label_ss == 'stop sign' and self.x_ss > 700:
+        if self.label_ss == 'stop sign':
             return 'STOP'
         else:
             return 'GO'
@@ -84,6 +119,7 @@ class StopDetector(object):
         # Publishing again the image to yolo
         # Note: This is not the way to do it, but we are 
         # doing it because we are running out of time
+        self.img2yolo = img
         self.img2yolo_pub.publish(img)
 
     def run(self):
